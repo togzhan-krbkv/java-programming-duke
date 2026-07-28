f686fda4b77820ce5acd4e1b42fe0360ed3cf4f4
togzhan <kairbekova020202@gmail.com>
2026-07-28 23:47:33 +0500

    Add files

---
+++ course-4-principles-of-software-design/module-4-ngrams-predictive-text/WordNGrams/IMarkovModel.java
@@ -0,0 +1,15 @@
+/**
+ * IMarkovModel, common interface for word level text generation models
+ * trained on a body of text, so that different orders can be used
+ * interchangeably.
+ *
+ * @author Togzhan K.
+ */
+public interface IMarkovModel
+{
+    void setTraining(String text);
+
+    void setRandom(int seed);
+
+    String getRandomText(int numChars);
+}
