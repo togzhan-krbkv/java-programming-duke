95665b4b763b8e0f8641723a58309cd0f6857621
togzhan <kairbekova020202@gmail.com>
2026-07-28 23:35:46 +0500

    Add files

---
+++ course-4-principles-of-software-design/module-4-ngrams-predictive-text/RandomText/MarkovFour.java
@@ -0,0 +1,78 @@
+/**
+ * MarkovFour, a fixed order 4 Markov model: generates each character based
+ * on the 4 characters that precede it in the training text. This is the
+ * standalone version before the code was refactored to share a common
+ * interface and abstract base class.
+ *
+ * @author Togzhan K.
+ */
+import java.util.Random;
+import java.util.ArrayList;
+
+public class MarkovFour
+{
+    private String myText;
+    private Random myRandom;
+
+    public MarkovFour()
+    {
+        myRandom = new Random();
+    }
+
+    public void setRandom(int seed)
+    {
+        myRandom = new Random(seed);
+    }
+
+    public void setTraining(String s)
+    {
+        myText = s.trim();
+    }
+
+    public String getRandomText(int numChars)
+    {
+        if (myText == null)
+        {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int index = myRandom.nextInt(myText.length() - 4);
+        String key = myText.substring(index, index + 4);
+        sb.append(key);
+
+        for (int k = 0; k < numChars - 4; k++)
+        {
+            ArrayList<String> followList = getFollows(key);
+            if (followList.size() == 0)
+            {
+                break;
+            }
+            index = myRandom.nextInt(followList.size());
+            String next = followList.get(index);
+            sb.append(next);
+            key = key.substring(1) + next;
+        }
+
+        return sb.toString();
+    }
+
+    public ArrayList<String> getFollows(String key)
+    {
+        ArrayList<String> result = new ArrayList<String>();
+        int startPos = 0;
+
+        while (true)
+        {
+            int index = myText.indexOf(key, startPos);
+            if (index == -1 || index == myText.length() - key.length())
+            {
+                break;
+            }
+            result.add(myText.substring(index + key.length(), index + key.length() + 1));
+            startPos = index + key.length();
+        }
+
+        return result;
+    }
+}
